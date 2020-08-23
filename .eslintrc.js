c6eab4970994fd2aaaafba33b2cacb4b876841ce
Oleksandr <skrypka.o.v@gmail.com>
2020-08-23 12:12:30 +0300

    Configured ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,15 @@ module.exports = {
   extends: [
     'airbnb-base',
     'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended'
+    'plugin:jest/recommended',
+    'eslint:recommended'
   ],
   plugins: ['@typescript-eslint', 'jest'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.eslint.json',
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
   env: {
     node: true,
@@ -57,7 +60,19 @@ module.exports = {
     'arrow-parens': ['error', 'as-needed'],
     'operator-linebreak': ['error', 'after'],
     'no-console': 'off',
-    'comma-dangle': ['error', 'only-multiline']
+    'comma-dangle': ['error', 'only-multiline'],
+    indent: ['warn', 2, {
+      SwitchCase: 1,
+      flatTernaryExpressions: true
+    }],
+    'no-constant-condition': ['error', {
+      checkLoops: false
+    }],
+    'no-debugger': 'off',
+    'no-labels': ['error', {
+      allowLoop: true
+    }],
+    semi: [2, 'always']
   },
   overrides: [
     {
